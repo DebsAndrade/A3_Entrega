3f74bd3c475627cc33d27a323bb4f2c013bb74e4
doandrade <doandrade@tce.pb.gov.br>
2025-11-06 15:17:02 +0000

    feat: Cria arquivo javascript com informações.

---
+++ info.js
@@ -0,0 +1,11 @@
+// Minha comida favorita:
+console.log("Minha comida favorita é risotto.");
+
+// Minha bebida favorita:
+console.log("Minha bebida favorita é água.");
+
+// Minha cor favorita:
+console.log("Minha cor favorita é roxo.");
+
+// Meu hobby favorito:
+console.log("Meu hobby favorito é ler livros e assistir séries/filmes.");
